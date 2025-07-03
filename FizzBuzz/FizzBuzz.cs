b89fec3bbc7d34cbbbbea515b9d3d76315578dab
skyler dennis <skylerdenniss11@gmail.com>
2025-07-03 18:07:24 -0400

    Finnished the white board project

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -5,27 +5,32 @@
 // when the number is divisible by both, return the word fizzbuzz
 
 
-public string FizzBuzz(int number)
-{
-    var answer = "";
-
-    if(number % 3 == 0)
+    public static void Main(string[] args)
     {
-        answer = "Fizz"
+        public string FizzBuzz(int number)
+        {
+            var answer = "";
 
+            if (number % 3 == 0)
+            {
+                answer = "Fizz"
+                
+            }
+    
+
+
+            if (number % 5 == 0)
+            {
+                answer += "Buzz"
+       
+    
+                return answer;
+            }
+
+
+
+        }
     }
 
 
-     if(number % 5 == 0)
-    {
-        answer += "Buzz"
-
-    }
-    return answer;
-
-
-   
-
-}
-
-
+    
